9bb871f868123a0f8c6afa2099fa429479749dd2
Joscha Feth <joscha@feth.com>
2015-01-20 15:05:14 +0100

    do not use make check

--- deps.sh
+++ deps.sh
@@ -9,7 +9,7 @@ tar zxvf bcm2835-$bcm2385_version.tar.gz
 cd bcm2835-$bcm2385_version
 ./configure
 make
-sudo make check
+# sudo make check
 sudo make install
 rm -rf ./bcm2835-$bcm2385_version
 echo "BCM235 library installed"
