e96a018299d727bc5510bcef5ca32f945f2a554c
Joscha Feth <joscha@feth.com>
2015-01-20 23:41:32 +0100

    push to firebase

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Based on [Resin.io](http://resin.io).
 Environment variables you can set in your resin.io dashboard:
 
 * `SENSOR_INTERVAL`: The interval (in seconds) in which to query the sensor for data.
+* `FIREBASE_URL`: The url to your firebase
+* `SENSOR_TYPE`: The sensor type, defaults to `22`
+* `SENSOR_PIN`: The sensor gpio pin, defaults to `4`
--- index.js
+++ index.js
@@ -6,17 +6,51 @@
   * Defaults to 5 minutes (300).
   */
   var interval = process.env.SENSOR_INTERVAL || 5*60;
+  var resinDeviceUuid = process.env.RESIN_DEVICE_UUID;
+  var firebaseUrl = process.env.FIREBASE_URL;
+  var sensorType = process.env.SENSOR_TYPE || 22;
+  var sensorPin = process.env.SENSOR_PIN || 4;
+  var firebaseSecret = process.env.FIREBASE_SECRET;
 
+  if (!firebaseSecret) {
+      console.error('FIREBASE_SECRET is needed');
+  }
+
+  var FirebaseClient = require('firebase-client');
   var sensorLib = require('node-dht-sensor');
+  var moment = require('moment');
+
+  var firebase = new FirebaseClient({
+      url: firebaseUrl,
+      auth: firebaseSecret
+  });
 
   var sensor = {
     initialize: function() {
-      return sensorLib.initialize(22, 4);
+      return sensorLib.initialize(sensorType, sensorPin);
     },
     read: function() {
       var readout = sensorLib.read();
-      console.log('Temperature: ' + readout.temperature.toFixed(2) + 'C, ' +
-      'humidity: ' + readout.humidity.toFixed(2) + '%');
+      var temperature = readout.temperature.toFixed(2);
+      var humidity = readout.humidity.toFixed(2);
+
+      console.log(moment().format(), 'Temperature: ' + temperature + 'C, ' +
+      'humidity: ' + humidity + '%');
+
+      var path = '/' + [deviceName, 'readings'].join('/');
+      firebase
+          .push(path, {
+              temperature: temperature,
+              humidity: humidity,
+              timestamp: Firebase.ServerValue.TIMESTAMP
+          })
+          .then(function() {
+              console.log('Written data to firebase');
+          })
+          .fail(function(err) {
+              console.error(err);
+          });
+
       setTimeout(function() {
         sensor.read();
       }, interval * 1000);
