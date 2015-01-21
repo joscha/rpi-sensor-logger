31bd3af147811d3caf80d59bb1806a991dadf8e2
Joscha Feth <joscha@feth.com>
2015-01-21 11:43:39 +0100

    housekeeping

--- README.md
+++ README.md
@@ -1,10 +1,24 @@
 # RPi sensor logger
 
-Based on [Resin.io](http://resin.io).
+This [resin.io](http://resin.io) based Raspberry pi node application reads temperature and humidity information from a sensor (DHT11/22 or AM2302) attached to the Raspberry pi and pushes the data to [firebase](http://www.firebase.com) in a given interval.
+
+Borrows from [resin.io/basic-resin-node-project](https://github.com/resin-io/basic-resin-node-project) and [shaunmulligan/firebaseDTL](https://github.com/shaunmulligan/firebaseDTL).
+
+Requirements:
+* A Raspberry pi
+* A DHT11, DHT22 or AM2032 sensor
+* A resin.io account
+* A firebase account
 
 Environment variables you can set in your resin.io dashboard:
 
-* `SENSOR_INTERVAL`: The interval (in seconds) in which to query the sensor for data.
-* `FIREBASE_URL`: The url to your firebase
-* `SENSOR_TYPE`: The sensor type, defaults to `22`
-* `SENSOR_PIN`: The sensor gpio pin, defaults to `4`
+Required:
+
+* `FIREBASE_URL`: The url to your firebase.
+* `FIREBASE_SECRET`: The firebase secret.
+
+Optional:
+
+* `SENSOR_INTERVAL`: The interval (in seconds) in which to query the sensor for data. Defaults to `300` (5min).
+* `SENSOR_TYPE`: The sensor type, defaults to `22`. Possible values: DHT11:  `11`, DHT22:  `22`, AM2302: `22`.
+* `SENSOR_PIN`: The sensor gpio pin, defaults to `4`.
--- index.js
+++ index.js
@@ -4,16 +4,52 @@
   /**
   * @var {Number} the interval to gather the sensor output in seconds.
   * Defaults to 5 minutes (300).
+  * You can set this through the resion.io dashboard.
   */
   var interval = process.env.SENSOR_INTERVAL || 5*60;
+
+  /**
+  * @var {String} the resin.io device ID. This is automatically set.
+  */
   var resinDeviceUuid = process.env.RESIN_DEVICE_UUID;
+
+  /**
+  * @var {String} the URL to the firebase. Required.
+  * Set this through the resin.io dashboard.
+  */
   var firebaseUrl = process.env.FIREBASE_URL;
+
+  /**
+  * @var {int} the sensor type to read from. Defaults to 22.
+  * You can set this through the resion.io dashboard.
+  *
+  * Possible values are:
+  *
+  * DHT11:  11
+  * DHT22:  22
+  * AM2302: 22
+  */
   var sensorType = process.env.SENSOR_TYPE || 22;
+
+  /**
+  * @var {int} the sensor gpio pin number. Defaults to 4.
+  * You can set this through the resion.io dashboard.
+  */
   var sensorPin = process.env.SENSOR_PIN || 4;
+
+  /**
+  * @var {String} the firebase secret. Required.
+  */
   var firebaseSecret = process.env.FIREBASE_SECRET;
+
+  if (!firebaseUrl) {
+      console.error('FIREBASE_URL is needed');
+      process.exit(1);
+  }
 
   if (!firebaseSecret) {
       console.error('FIREBASE_SECRET is needed');
+      process.exit(1);
   }
 
   var FirebaseClient = require('firebase-client');
@@ -27,7 +63,10 @@
 
   var sensor = {
     initialize: function() {
-      return sensorLib.initialize(parseInt(sensorType, 10), parseInt(sensorPin, 10));
+      return sensorLib.initialize(
+          parseInt(sensorType, 10),
+          parseInt(sensorPin, 10)
+      );
     },
     read: function() {
       var data = sensorLib.read();
@@ -50,9 +89,6 @@
                   temperature: +temperature,
                   humidity: +humidity,
                   timestamp: timestamp
-              })
-              .then(function() {
-                  console.log('Written to firebase');
               })
               .fail(function(err) {
                   console.error(err);
